0deb719a50ca4c1d0b885994989db386ea09bb72
Grigory Piskunov <unixfan@yandex.ru>
2025-12-23 16:28:32 +0100

    add ingestion service minimum run

---
+++ ingestion-service/src/main/java/com/example/IngestionService.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestionService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(IngestionService.class, args);
+
+	}
+
+}
